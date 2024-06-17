afbf116f9d20917800ac961f88a732b7744cf280
Abhishek Kumar <115798048+abhiyoke@users.noreply.github.com>
2024-06-17 11:24:25 +0530

    Create no. of ways to arrive at dest in shortest amount of time.cpp

---
+++ dijkstra/no. of ways to arrive at dest in shortest amount of time.cpp
@@ -0,0 +1,55 @@
+/*
+You are given an integer n and a 2D integer array ‘roads’ where roads[i] = [ui, vi, timei] means that there is a road between intersections ui and vi
+that takes timei minutes to travel. You want to know in how many ways you can travel from intersection 0 to intersection n - 1 in the shortest amount of time.
+Return the number of ways you can arrive at your destination in the shortest amount of time. Since the answer may be large, return it modulo 109 + 7.
+  */
+
+/*
+esme bs hme ek ways array lena h jo ki uss node tk pahuchne ka count btaega toh if(d+wt<dist[adj]) then ways[adj]=ways[node] aur agar equal hoga then 
+ways[adj]++.return kr denge ways[n-1].
+  */
+class Solution
+{
+public:
+    int countPaths(int n, vector<vector<int>> &roads)
+    {
+        // Creating an adjacency list for the given graph.
+        vector<pair<int, int>> adj[n];
+        for (auto it : roads)
+        {
+            adj[it[0]].push_back({it[1], it[2]});
+            adj[it[1]].push_back({it[0], it[2]});
+        }
+        priority_queue<pair<int, int>,
+                       vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        vector<int> dist(n, INT_MAX), ways(n, 0);
+        dist[0] = 0;
+        ways[0] = 1;
+        pq.push({0, 0});
+        int mod = (int)(1e9 + 7);
+        while (!pq.empty())
+        {
+            int dis = pq.top().first;
+            int node = pq.top().second;
+            pq.pop();
+
+            for (auto it : adj[node])
+            {
+                int adjNode = it.first;
+                int edW = it.second;
+                if (dis + edW < dist[adjNode])
+                {
+                    dist[adjNode] = dis + edW;
+                    pq.push({dis + edW, adjNode});
+                    ways[adjNode] = ways[node];
+                }
+
+                else if (dis + edW == dist[adjNode])
+                {
+                    ways[adjNode] = (ways[adjNode] + ways[node]) % mod;
+                }
+            }
+        }
+        return ways[n - 1] % mod;
+    }
+};
