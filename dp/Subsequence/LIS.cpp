c53c00518c3065a122612cbd04cf0d29b21b5d55
Abhishek Kumar <115798048+abhiyoke@users.noreply.github.com>
2024-06-19 21:31:04 +0530

    Update LIS.cpp

--- dp/Subsequence/LIS.cpp
+++ dp/Subsequence/LIS.cpp
@@ -38,3 +38,30 @@ public:
     return dp[0][0];
     }
 };
+
+
+/*
+int longestIncreasingSubsequence(int arr[], int n){
+    
+    vector<int> dp(n,1);
+    
+    for(int i=0; i<=n-1; i++){
+        for(int prev_index = 0; prev_index <=i-1; prev_index ++){
+            
+            if(arr[prev_index]<arr[i]){
+                dp[i] = max(dp[i], 1 + dp[prev_index]);
+            }
+        }
+    }
+    
+    int ans = -1;
+    
+    for(int i=0; i<=n-1; i++){
+        ans = max(ans, dp[i]);
+    }
+    
+    return ans;
+}
+
+
+*/
