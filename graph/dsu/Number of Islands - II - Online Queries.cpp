b9d53eee4c1ad0a3d89f492c1273f81bcd370c20
Abhishek Kumar <115798048+abhiyoke@users.noreply.github.com>
2024-06-19 12:29:40 +0530

    Create Number of Islands - II - Online Queries.cpp

---
+++ graph/dsu/Number of Islands - II - Online Queries.cpp
@@ -0,0 +1,114 @@
+/*
+You are given an n, m which means the row and column of the 2D matrix, and an array of size k denoting the number of operations. 
+Matrix elements are 0 if there is water or 1 if there is land. Originally, the 2D matrix is all 0 which means there is no land in the matrix. The array has
+k operator(s) and each operator has two integers A[i][0], A[i][1] means that you can change the cell matrix[A[i][0]][A[i][1]] from sea to island. Return how many
+islands are there in the matrix after each operation. 
+You need to return an array of size k.
+*/
+
+/*
+toh esme mujhe check krenge ki agar visited h toh just cnt daldo ans vector mai ya uske 4 dir mai dekho ki visited h ya nai aur enhe nodes mai convert
+krenge like row*m+col.Aur charo dir mai jane se pehle hm cnt++ kr denge.Then check krenge ki particluar dir visited h ya nai and agar h then
+hm check krenge ki adj and node k parent same h ya mai means ki voh pehle se connected h ya nai.And agar nai h toh hm cnt-- krenge.THen use hm
+ans mai dal denge.
+  */
+#include <bits/stdc++.h>
+using namespace std;
+
+// User function Template for C++
+class DisjointSet {
+    vector<int> rank, parent, size;
+public:
+    DisjointSet(int n) {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        size.resize(n + 1);
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    int findUPar(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+        }
+        else if (rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u;
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+class Solution {
+private:
+    bool isValid(int adjr, int adjc, int n, int m) {
+        return adjr >= 0 && adjr < n && adjc >= 0 && adjc < m;
+    }
+public:
+    vector<int> numOfIslands(int n, int m,
+                             vector<vector<int>> &operators) {
+        DisjointSet ds(n * m);
+        int vis[n][m];
+        memset(vis, 0, sizeof vis);
+        int cnt = 0;
+        vector<int> ans;
+        for (auto it : operators) {
+            int row = it[0];
+            int col = it[1];
+            if (vis[row][col] == 1) {
+                ans.push_back(cnt);
+                continue;
+            }
+            vis[row][col] = 1;
+            cnt++;
+            // row - 1, col
+            // row , col + 1
+            // row + 1, col
+            // row, col - 1;
+            int dr[] = { -1, 0, 1, 0};
+            int dc[] = {0, 1, 0, -1};
+            for (int ind = 0; ind < 4; ind++) {
+                int adjr = row + dr[ind];
+                int adjc = col + dc[ind];
+                if (isValid(adjr, adjc, n, m)) {
+                    if (vis[adjr][adjc] == 1) {
+                        int nodeNo = row * m + col;
+                        int adjNodeNo = adjr * m + adjc;
+                        if (ds.findUPar(nodeNo) != ds.findUPar(adjNodeNo)) {
+                            cnt--;
+                            ds.unionBySize(nodeNo, adjNodeNo);
+                        }
+                    }
+                }
+            }
+            ans.push_back(cnt);
+        }
+        return ans;
+    }
+};
