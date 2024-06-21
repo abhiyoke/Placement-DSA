6193fb99031a2cd9d5ae90c7250585ed1ec22236
Abhishek Kumar <115798048+abhiyoke@users.noreply.github.com>
2024-06-21 13:17:40 +0530

    Update matrix chain multiplication.cpp

--- dp/partition dp/matrix chain multiplication.cpp
+++ dp/partition dp/matrix chain multiplication.cpp
@@ -26,3 +26,25 @@ public:
         
     }
 };
+
+//Tabulation 
+
+int matrixMultiplication(vector<int> &arr, int N)
+{
+      int dp [N] [N];
+      for (int i = 1;i<N;i++) dp[i][i] = 0;
+      for (int i = N-1;i>=1; i--) {
+            for(int j = i;j<N;j++) {
+                  int mini = 1e9;
+                  for (int k = i; k < j; k++) {
+                        int steps = arr[i-1] * arr[k] * arr[j]
+                              + dp[i] [k]
+                              + dp[k+1][j];
+                        if (steps mini) mini = steps;
+                  }
+                  dp[i][j] = mini;
+            }
+      }
+      return dp[1] [N-1];
+      // Write your code here.
+}
