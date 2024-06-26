ec116ceab74221ce79308321888d405207a1267b
Abhishek Kumar <115798048+abhiyoke@users.noreply.github.com>
2024-06-26 12:06:53 +0530

    Create 6viii.bottom view of binary tree.cpp

---
+++ Tree/6viii.bottom view of binary tree.cpp
@@ -0,0 +1,42 @@
+ vector <int> bottomView(Node *root) {
+        // Your Code Here
+          vector<int> res;
+    if (root == NULL)
+        return res;
+    
+    queue<pair<Node*, int>> q; // Queue for level order traversal with horizontal distance
+    map<int, int> m; // Map to store nodes at each vertical distance
+    
+    q.push({root, 0}); // Push the root node with horizontal distance 0
+    
+    while (!q.empty()) {
+        auto it = q.front();
+        q.pop();
+        
+        Node* node = it.first;
+        int vertical = it.second;
+        
+        // If the current vertical distance is not present in the map,
+        // add the node's data to the map for that vertical distance
+
+            m[vertical] = node->data;
+        
+        
+        // Process the left child if it exists, with a reduced vertical distance
+        if (node->left != NULL) {
+            q.push({node->left, vertical - 1});
+        }
+        
+        // Process the right child if it exists, with an increased vertical distance
+        if (node->right != NULL) {
+            q.push({node->right, vertical + 1});
+        }
+    }
+    
+    // Append the values from the map to the result vector in sorted order
+    for (auto it : m) {
+        res.push_back(it.second);
+    }
+    
+    return res;
+    }
