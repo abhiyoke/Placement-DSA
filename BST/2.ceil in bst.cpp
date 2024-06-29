655738435b5b6370d4753b91ead0e53e2acf18a6
Abhishek Kumar <115798048+abhiyoke@users.noreply.github.com>
2024-06-29 18:14:12 +0530

    Create 2.ceil in bst.cpp

---
+++ BST/2.ceil in bst.cpp
@@ -0,0 +1,30 @@
+int findCeil(TreeNode* root, int key){
+        // Initialize the variable
+        // to store the ceiling value
+        int ceil = -1; 
+        
+        // Traverse the BST until reaching
+        // the end or finding the key
+        while(root){
+             // If the key is found, assign it
+             // as the ceiling and return
+            if(root->val == key){
+                ceil = root->val;
+                return ceil;
+            } 
+            
+             // If the key is greater,
+             // move to the right subtree
+            if(key > root->val){
+                root = root->right;
+            }
+            else{ 
+                // If the key is smaller, update ceil
+                // and move to the left subtree
+                ceil = root->val;
+                root = root->left;
+            }
+        }
+         // Return the computed ceiling value
+        return ceil;
+    }
