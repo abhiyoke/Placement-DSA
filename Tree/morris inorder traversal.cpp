7158da38b5b2df353a967280afa6fb1d8324fb55
Abhishek Kumar <115798048+abhiyoke@users.noreply.github.com>
2024-06-29 15:11:16 +0530

    Create morris inorder traversal.cpp

---
+++ Tree/morris inorder traversal.cpp
@@ -0,0 +1,28 @@
+//tc:o(n)
+//sc:O(1)
+vector<int> inorderTraversal(TreeNode* root) {
+        vector<int>inorder;
+        TreeNode* curr=root;
+        while(curr!=nullptr){
+            if(curr->left==nullptr){
+                inorder.push_back(curr->val);
+                curr=curr->right;
+            }
+            else{
+                TreeNode* prev=curr->left;
+                while(prev->right && prev->right!=curr){
+                    prev=prev->right;
+                }
+                if(prev->right==nullptr){
+                    prev->right=curr;
+                    curr=curr->left;
+                }
+                else{
+                    prev->right=nullptr;
+                    inorder.push_back(curr->val);
+                    curr=curr->right;
+                }
+            }
+        }
+        return inorder;
+    }
