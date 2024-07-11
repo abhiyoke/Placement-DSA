c9ef591237a89bfce8197498e7d94a6261e21ce1
Abhishek Kumar <115798048+abhiyoke@users.noreply.github.com>
2024-07-11 22:13:39 +0530

    Update sql.sql

--- DBMS/sql.sql
+++ DBMS/sql.sql
@@ -126,3 +126,21 @@ WITH OrderedQueue AS (
         Queue
 )
 
+24.SELECT
+    'Low Salary' AS category,
+    COUNT(*) AS accounts_count
+FROM Accounts
+WHERE income < 20000
+UNION ALL
+SELECT
+    'Average Salary' AS category,
+    COUNT(*) AS accounts_count
+FROM Accounts
+WHERE income >= 20000 AND income <= 50000
+UNION ALL
+SELECT
+    'High Salary' AS category,
+    COUNT(*) AS accounts_count
+FROM Accounts
+WHERE income > 50000;
+
