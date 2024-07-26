93de7941013b81a00bc5f12e2404906999c02abe
Abhishek Kumar <115798048+abhiyoke@users.noreply.github.com>
2024-07-26 11:27:28 +0530

    Update linked_list.cpp

--- Linked List/linked_list.cpp
+++ Linked List/linked_list.cpp
@@ -1,1 +1,54 @@
+//Class of node
+class Node {
+public:
+    int data;
+    Node* next;
+    Node(int data1, Node* next1) {
+        data = data1;
+        next = next1;
+    }
+    Node(int data1) {
+        data = data1;
+        next = nullptr;
+    }
+};
+
+//insert a node
+void insertAtBeginning(Node*& head, int newData) {
+    Node* newNode = new Node(newData, head);
+    head = newNode;
+}
+void insertAtEnd(Node*& head, int newData) {
+    Node* newNode = new Node(newData);
+
+    if (head == nullptr) {
+        head = newNode;
+        return;
+    }
+    Node* temp = head;
+    while (temp->next != nullptr) {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+}
+void insertAtMiddle(Node*& head, int newData, int position) {
+    Node* newNode = new Node(newData);
+    if (position == 1) {
+        newNode->next = head;
+        head = newNode;
+        return;
+    }
+   Node* temp = head;
+    for (int i = 1; i < position - 1 && temp != nullptr; ++i) {  //imp
+        temp = temp->next;
+    }
+    if (temp == nullptr) {
+        cout << "Position out of range" << endl;
+        delete newNode;
+        return;
+    }
+
+    newNode->next = temp->next;  //imp
+    temp->next = newNode;  //imp
+}
 
