214bd84ada3cf5a7d051872e34ea8adb31e65ce5
Abhishek Kumar <115798048+abhiyoke@users.noreply.github.com>
2024-07-28 16:15:44 +0530

    Update linked_list.cpp

--- Linked List/linked_list.cpp
+++ Linked List/linked_list.cpp
@@ -393,6 +393,20 @@ Node* deleteAllOccurrences(Node* head, int k) {
             temp=temp->next;
         } return head; }
 
+//Rotate LL:
+ ListNode* rotateRight(ListNode* head, int k) {
+        if(head==nullptr || head->next==nullptr || k==0) return head;
+        int len=1;
+        ListNode* temp=head;
+        while(temp->next!=nullptr){
+           len++;
+            temp=temp->next;
+        }
+        temp->next=head;  k=k%len;  int n=len-k;
+       while(n--) temp=temp->next;
+       head=temp->next;  temp->next=nullptr;
+       return head; }
+
 
 
 
