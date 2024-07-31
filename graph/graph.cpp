50209891b94d8a87db33b48d86f19fbd3140dce3
Abhishek Kumar <115798048+abhiyoke@users.noreply.github.com>
2024-07-31 23:18:39 +0530

    Create graph.cpp

---
+++ graph/graph.cpp
@@ -0,0 +1,135 @@
+//bfs
+vector<int> bfsOfGraph(int V, vector<int> adj[]) {
+        int vis[V] = {0}; 
+        vis[0] = 1; 
+        queue<int> q;
+        q.push(0); 
+        vector<int> bfs; 
+        while(!q.empty()) {
+           // get the topmost element in the queue 
+            int node = q.front(); 
+            q.pop(); 
+            bfs.push_back(node); 
+            // traverse for all its neighbours 
+            for(auto it : adj[node]) {
+                // if the neighbour has previously not been visited, 
+                // store in Q and mark as visited 
+                if(!vis[it]) {
+                    vis[it] = 1; 
+                    q.push(it); 
+                }
+            }
+        }
+        return bfs; 
+    }
+
+//dfs
+void dfs(int node, vector<int> adj[], int vis[], vector<int> &ls) {
+        vis[node] = 1; 
+        ls.push_back(node); 
+        // traverse all its neighbours
+        for(auto it : adj[node]) {
+            // if the neighbour is not visited
+            if(!vis[it]) {
+                dfs(it, adj, vis, ls); 
+            }
+        }
+    }
+
+//detect cycle in undirected graph
+bool detcycle(int st,vector<int>adj[],int vis[]){
+      queue<pair<int,int>>q;
+      vis[st]=1;
+      q.push({st,-1});
+      while(!q.empty()){
+          int par=q.front().second;
+          int node=q.front().first;
+          q.pop();
+          for(auto it:adj[node]){
+              if(!vis[it]){
+                  vis[it]=1;
+                  q.push({it,node});
+              }
+              else if(par!=it) return true;
+          }
+        
+      }
+        return false;
+  }
+    // Function to detect cycle in an undirected graph.
+    bool isCycle(int n, vector<int> adj[]) {
+        // Code here
+        int vis[n]={0};
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+              if(detcycle(i,adj,vis)){
+                  return true;
+              }
+            }
+        }
+        return false;
+    }
+
+//bipartite graph(no two adj node have same colour)
+  bool dfs(int node, int col, int color[], vector<int> adj[]) {
+        color[node] = col; 
+        
+        // traverse adjacent nodes
+        for(auto it : adj[node]) {
+            // if uncoloured
+            if(color[it] == -1) {
+                if(dfs(it, !col, color, adj) == false) return false; 
+            }
+            // if previously coloured and have the same colour
+            else if(color[it] == col) {
+                return false; 
+            }
+        }
+        
+        return true; 
+    }
+	bool isBipartite(int V, vector<int>adj[]){
+	    int color[V];
+	    for(int i = 0;i<V;i++) color[i] = -1; 
+	    // for connected components
+	    for(int i = 0;i<V;i++) {
+	        if(color[i] == -1) {
+	            if(dfs(i, 0, color, adj) == false) 
+	                return false; 
+	        }
+	    }
+	    return true; 
+	}
+
+//detect cycle in directed graph
+bool dfsCheck(int node, vector<int> adj[], int vis[], int pathVis[]) {
+		vis[node] = 1;
+		pathVis[node] = 1;
+
+		// traverse for adjacent nodes
+		for (auto it : adj[node]) {
+			// when the node is not visited
+			if (!vis[it]) {
+				if (dfsCheck(it, adj, vis, pathVis) == true)
+					return true;
+			}
+			// if the node has been previously visited
+			// but it has to be visited on the same path
+			else if (pathVis[it]) {
+				return true;
+			}
+		}
+		pathVis[node] = 0;
+		return false;
+	}
+	bool isCyclic(int V, vector<int> adj[]) {
+		int vis[V] = {0};
+		int pathVis[V] = {0};
+
+		for (int i = 0; i < V; i++) {
+			if (!vis[i]) {
+				if (dfsCheck(i, adj, vis, pathVis) == true) return true;
+			}
+		}
+		return false;
+	}
